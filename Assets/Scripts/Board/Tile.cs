60b61ee4e975c214a3cd2090660de76d79c9da68
KeMaKone <atpgamer@gmail.com>
2021-08-15 15:17:56 +0300

    Added List<Vector3>

--- Assets/Scripts/Board/Tile.cs
+++ Assets/Scripts/Board/Tile.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 
 //TODO add logic to bind with a tileObject
 public class Tile : MonoBehaviour{
-    private int tileID;
+    protected int tileID;
+    private List<Vector3> vector3PlayerLocations;
+
+    public Tile(int tileID, Vector3 location1, Vector3 location2, Vector3 location3, Vector3 location4){
+        this.tileID = tileID;
+        vector3PlayerLocations = new List<Vector3>(){
+            location1, location2, location3, location4
+        };
+    }
     
 
     public int GetID(){
         return tileID;
+    }
+
+    //returns a list of vector3 playerlocation
+    public List<Vector3> GetPlayerLocations(){
+        return vector3PlayerLocations;
     }
 }
