2cb804f60f59e5de15983a63f41db8ee9abdec95
Valtteri Aarnio <github@vaarn.io>
2022-01-08 19:20:02 +0200

    Moved building instantiating from BuildHouse to Build. Created wrapper methods BuildHouse and BuildHotel that call Build with desired prefab. Simplified building positioning.

--- Assets/Scripts/Board/PropertyTile.cs
+++ Assets/Scripts/Board/PropertyTile.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PropertyTile : Tile{
-    [SerializeField] GameObject housePrefab;
+    [SerializeField] GameObject housePrefab; //has tag 'House'
+    [SerializeField] GameObject hotelPrefab; //has tag 'Hotel'
     [SerializeField] int value;
     [SerializeField] int district; //0-7
 
@@ -12,41 +13,24 @@ public class PropertyTile : Tile{
     }
 
     public void BuildHouse(){
-        //TODO build hotel
-        //TODO check if house is already built on tile
-        Transform tileTransform = GetTileTransfrom();
+        Build(housePrefab);
+    }
+
+    public void BuildHotel(){
+        Build(hotelPrefab);
+    }
+
+    public void Build(GameObject prefab){
+        //TODO check if something is already built on the tile
+        Transform transform = GetTileTransfrom();
         Vector3 position = transform.position;
         Quaternion rotation = transform.rotation;
-        Vector3 eulerAngles = rotation.eulerAngles;
-        Debug.Log(eulerAngles);
 
-        float offsetZ = 0;
-        float offsetX = 0;
+        GameObject building = Instantiate(prefab, position, rotation);
+        building.transform.SetParent(transform);
 
-        switch(eulerAngles.y){
-            case 0:
-                offsetZ = 5f;
-                offsetX = 0f;
-                break;
-            case 90:
-                offsetZ = 0f;
-                offsetX = 5f;
-                break;
-            case 180:
-                offsetZ = -5F;
-                offsetX = 0f;
-                break;
-            case 270:
-                offsetZ = 0f;
-                offsetX = -5f;
-                break;
-        }
-
-        position.y = position.y + 1;
-        position.x = position.x + offsetX;
-        position.z = position.z + offsetZ;
-        GameObject house = Instantiate(housePrefab, position, rotation);
-        house.transform.parent = tileTransform;
+        //Move to top edge of the tile, take into account parent scaling.
+        building.transform.localPosition = new Vector3(0f, 0.5f, 0.25f);
     }
 
     //public getters
